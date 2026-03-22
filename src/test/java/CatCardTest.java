4533ad00658e517c14456af7be1c79b9512e82a3
mariaaakat <mariaaa221191!@gmail.com>
2026-03-22 13:47:19 +0300

    писала автотесты на endpoint /api/v1/cat{1} Чек лист:      * 1.Проверка статус коа ответа при успехе (200)      * 2.Проверка формата ответа      * 3.Проверка существующего id      * 4.Проверка несуществующего id      * 5.Проверка id не с числовым форматом      * 6.Проверка id отрицательного значения      * 7.Проверка структуры ответа

---
+++ src/test/java/CatCardTest.java
@@ -0,0 +1,106 @@
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.notNullValue;
+
+public class CatCardTest {
+    public static final org.hamcrest.Matcher<Object> MATCHER = notNullValue();
+    public static String endpoint = "http://localhost:8081/api/cats/";
+    public static String endpointv1 = "http://localhost:8081/api/v1/cats/";
+    /**
+     * Чек лист:
+     * 1.Проверка статус коа ответа при успехе (200)
+     * 2.Проверка формата ответа
+     * 3.Проверка существующего id
+     * 4.Проверка несуществующего id
+     * 5.Проверка id не с числовым форматом
+     * 6.Проверка id отрицательного значения
+     * 7.Проверка структуры ответа
+     */
+
+    @Test
+    @DisplayName("Проверка статус кода ответа")
+    public void CatCardStatusCodTest(){
+        given()
+                .when()
+                .get(endpointv1 + "1")
+                .then()
+                .statusCode(200);
+    }
+
+
+    @Test
+    @DisplayName("Проверка формата ответа")
+    public void CatCardFormatTest(){
+        given()
+                .when()
+                .get(endpointv1 + "1")
+                .then()
+                .log().body()
+                .statusCode(200)
+                .contentType(ContentType.JSON);
+    }
+
+    @Test
+    @DisplayName("Проверка формата ответа")
+    public void CatCardFormat2Test(){
+        given()
+                .when()
+                .get(endpointv1 + "1")
+                .then()
+                //.log().body()
+                .statusCode(200)
+                .contentType(ContentType.JSON)
+                .body("id", equalTo(1))
+                .body("name", notNullValue())
+                .body("age",notNullValue())
+                .body("color",notNullValue())
+                .body("breed",notNullValue())
+                .body("weight",notNullValue())
+                .body("vaccinated",notNullValue() )
+                .body("birthDate",notNullValue())
+                .body("ownerEmail",notNullValue())
+                .body("status",notNullValue())
+                .body("createdAt",notNullValue())
+                .body("updatedAt",notNullValue());
+    }
+    @Test
+    @DisplayName("Проверка несуществующего id")
+    public void CatCardNotIdTest(){
+        given()
+                .when()
+                .get(endpointv1 + "20")
+                .then()
+                .log().body()
+                .statusCode(404)
+                .contentType(ContentType.JSON);
+    }
+
+    @Test
+    @DisplayName("Проверка несуществующего id")
+    public void CatCardStringIdTest(){
+        given()
+                .when()
+                .get(endpointv1 + "Go")
+                .then()
+                .log().body()
+                .statusCode(400)
+                .contentType(ContentType.JSON);
+    }
+
+    @Test
+    @DisplayName("Проверка несуществующего id")
+    public void CatCardNegativeIdTest(){
+        given()
+                .when()
+                .get(endpointv1 + "-15")
+                .then()
+                .log().body()
+                .statusCode(400)
+                .contentType(ContentType.JSON);
+    }
+
+}
