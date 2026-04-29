a45f94a830939af600c0ef2405bb8d9486dfef5d
mariaaakat <mariaaa221191!@gmail.com>
2026-04-29 20:15:15 +0300

    удаление уже удаленного кота по ид

--- src/test/java/tests/CatDeleteTest.java
+++ src/test/java/tests/CatDeleteTest.java
@@ -5,16 +5,13 @@ import io.restassured.response.Response;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.EmptySource;
-import org.junit.jupiter.params.provider.NullSource;
 import org.junit.jupiter.params.provider.ValueSource;
 import utils.CatGenerator;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertInstanceOf;
 
 public class CatDeleteTest {
-
+ private static final CatApiClient client = new CatApiClient();
     /**
      * Чек-лист удаления кота:
      * 1.Удаления кота по ид
@@ -29,19 +26,17 @@ public class CatDeleteTest {
 @DisplayName("Удаление кота по существующему ид")
   @Test
   public void deleteCatById(){
-    CatApiClient client = new CatApiClient();
     Response createCat = client.createCat(CatGenerator.catGenerator());
     createCat.prettyPrint();
     Integer id = createCat.jsonPath().getInt("id");
     Response deleteCat = client.deleteCatByID(id);
-    assertEquals(204 , deleteCat.statusCode());
+    assertEquals(204 , deleteCat.statusCode(),"Удаление кота прошло не успешно");
 
 }
   @DisplayName("Удаление кота по несуществующему ид с типом int")
   @ParameterizedTest
-  @ValueSource(ints = {-2, 0, 1000})
+  @ValueSource(ints = {-2, 0, 1000, 99999999})
   public void deleteCatNotValidIdTest(int id) {
-    CatApiClient client = new CatApiClient();
     Response deleteResponse = client.deleteCatByID(id);
     assertEquals(404, deleteResponse.statusCode());
   }
@@ -51,10 +46,18 @@ public class CatDeleteTest {
 //  @EmptySource // используется для объектов которые могут содержать стринг или самих стрингов
  // @NullSource // используется для объектов которые могут содержать стринг или самих стрингов и для других ссылочных типов данных
   public void deleteCatNotIntIdTest(Integer id){
-    CatApiClient client = new CatApiClient();
     // Response createCatResponse = client.createCat(CatGenerator.generateBaseCat("Tom", 5, "red", "street", 5.5, true, "10.11.2020", "tom@mail.ru"));
     Response deleteResponse = client.deleteCatByID(id);
     assertEquals(404, deleteResponse.statusCode());
+  }
+
+  @DisplayName("Удаление уже удаленного кота")
+  @Test
+  public void deleteCatTwice(){
+  int id =  client.createCat(CatGenerator.catGenerator()).jsonPath().getInt("id");
+  assertEquals( 204, client.deleteCatByID(id).statusCode());
+  assertEquals( 404, client.deleteCatByID(id).statusCode());
+
   }
 
 
